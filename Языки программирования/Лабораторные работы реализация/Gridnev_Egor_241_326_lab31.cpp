77da4d4b0641b21fb7a6695b03130bdd35f2b2a7
Egor Gridnev <egnv.main@gmail.com>
2024-10-02 23:57:33 +0300

    create lab31 and create bubbleSort

---
+++ Языки программирования/Лабораторные работы реализация/Gridnev_Egor_241_326_lab31.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <limits>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::numeric_limits;
+using std::streamsize;
+
+
+void bubbleSort(vector<int>& arr) {
+
+    int size = arr.size();
+    
+    for (int i = 0; i < size - 1; i++)) {
+        for (int j = 0; j < size - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) ? swap(arr[j], arr[j + 1]);
+        }
+    }
+}
+
+
+
+
+int main() {
+
+
+
+
+    
+    return 0;
+}
