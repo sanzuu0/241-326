d20d05f267e82b100a0fcee2800bae6c40e252cd
Eg-mac <egnv.main@gmail.com>
2024-11-09 08:15:30 +0300

    конечный вид второй лабораторной

--- Языки программирования/Laboratory work 2/Gridnev_Egor_241_326_lab2.cpp
+++ Языки программирования/Laboratory work 2/Gridnev_Egor_241_326_lab2.cpp
@@ -1,31 +1,27 @@
 #include <iostream>
 #include <limits>
 
-using namespace std;
-
 // Отрисовка числового треугольника
 void numericalTriangle() {
-
     int height;
-    cout << "\nВведите положительное целое число для высоты треугольника: ";
+    std::cout << "\nВведите положительное целое число для высоты треугольника: ";
 
-    while (!(cin >> height) || height <= 0) {
-        cout << "Некорректный ввод. Пожалуйста, введите положительное целое число: ";
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    while (!(std::cin >> height) || height <= 0) {
+        std::cout << "Некорректный ввод. Пожалуйста, введите положительное целое число: ";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
 
     for (int i = 1; i <= height; i++) {
         for (int j = 1; j <= i; j++) {
-            cout << j << " ";
+            std::cout << j << " ";
         }
-        cout << endl;
+        std::cout << std::endl;
     }
 }
 
 // Вычисление биномиального коэффициента
 long long binomialCoefficient(int n, int k) {
-
     long long result = 1;
 
     if (k > n - k)
@@ -40,74 +36,76 @@ long long binomialCoefficient(int n, int k) {
 
 // Нахождение биномиальных коэффициентов
 void findingBinomialCoefficients() {
-
     int n;
-    cout << "\nВведите неотрицательное целое число для n: ";
+    std::cout << "\nВведите неотрицательное целое число для n: ";
 
-    while (!(cin >> n) || n < 0) {
-        cout << "Некорректный ввод. Пожалуйста, введите неотрицательное целое число: ";
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    while (!(std::cin >> n) || n < 0) {
+        std::cout << "Некорректный ввод. Пожалуйста, введите неотрицательное целое число: ";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
 
     for (int k = 0; k <= n; ++k) {
-        cout << "C(" << n << ", " << k << ") = " << binomialCoefficient(n, k) << endl;
+
+        if (binomialCoefficient(n, k) < 0) {
+            std::cout << "C(" << n << ", " << k << ") = Произошло переполнение" << std::endl;
+        } else {
+            std::cout << "C(" << n << ", " << k << ") = " << binomialCoefficient(n, k) << std::endl;
+        }
     }
 }
 
 // Вычисление среднего арифметического
 void calculatingTheArithmeticMean() {
-
     double sum = 0.0;
     int count = 0, element;
 
-    cout << "Введите координаты (целые числа) точек на линии (введите 0 для завершения): ";
+    std::cout << "Введите координаты (целые числа) точек на линии (введите 0 для завершения): ";
 
-    while (cin >> element && element != 0) {
+    while (std::cin >> element && element != 0) {
         sum += element;
         count++;
     }
-    cout << "Среднее арифметическое: " << (count == 0 ? 0 : sum / count) << endl;
+    std::cout << "Среднее арифметическое: " << (count == 0 ? 0 : sum / count) << std::endl;
 }
 
 int main() {
 
-    int choice;
+    int choice = 0;
 
-    do {
-        cout << "\n  1. Числовой треугольник.\n";
-        cout << "  2. Нахождение биномиальных коэффициентов.\n";
-        cout << "  3. Вычисление среднего арифметического для введенных координат точек на линии.\n";
-        cout << "  4. Выход.\n";
-        cout << "\nМеню программы, введите соответствующий номер для выбора действия: ";
-        cin >> choice;
-        cin.ignore();
+    while (choice != 4) {
+        std::cout << "\n  1. Числовой треугольник.\n";
+        std::cout << "  2. Нахождение биномиальных коэффициентов.\n";
+        std::cout << "  3. Вычисление среднего арифметического для введенных координат точек на линии.\n";
+        std::cout << "  4. Выход.\n";
+        std::cout << "\nМеню программы, введите соответствующий номер для выбора действия: ";
+        std::cin >> choice;
+        std::cin.ignore();
 
         switch (choice) {
-
             case 1:
-                cout << "Вы выбрали числовой треугольник.\n";
+                std::cout << "Вы выбрали числовой треугольник.\n";
                 numericalTriangle();
                 break;
 
             case 2:
-                cout << "Вы выбрали нахождение биномиальных коэффициентов.\n";
+                std::cout << "Вы выбрали нахождение биномиальных коэффициентов.\n";
                 findingBinomialCoefficients();
                 break;
 
             case 3:
-                cout << "Вы выбрали вычисление среднего арифметического.\n";
+                std::cout << "Вы выбрали вычисление среднего арифметического.\n";
                 calculatingTheArithmeticMean();
                 break;
 
             case 4:
-                cout << "Выход из программы.\n";
+                std::cout << "Выход из программы.\n";
                 break;
 
             default:
-                cout << "Неверный выбор, попробуйте снова.\n";
+                std::cout << "Неверный выбор, попробуйте снова.\n";
         }
-    } while (choice != 4);
+    }
 
     return 0;
 }
